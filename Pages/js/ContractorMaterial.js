0a90d2964cdf2c8719c287aa5d3e70d4d7248ff8
FireAwayH <hejiheji001@gmail.com>
2025-05-05 17:22:18 +0800

    update

---
+++ Pages/js/ContractorMaterial.js
@@ -0,0 +1,242 @@
+let init = false;
+
+let specialOrders = [
+    {
+        "data": "materialInfo",
+        "target": "contractor"
+    },
+    {
+        "data": "priceStr",
+        "target": "price"
+    },
+];
+const inputAction = function (requestData) {
+    if (!init) {
+        requestData.order = {};
+        requestData.sorting = "creationTime desc";
+    } else {
+        let sort = requestData.order[0];
+        let data = requestData.columns[sort.column].data;
+
+        specialOrders.forEach(o => {
+            if (o.data === data) {
+                requestData.order = {};
+                requestData.sorting = `${o.target} ${sort.dir}`;
+                console.log(requestData.sorting)
+            }
+        });
+    }
+    
+    requestData.startDate = startDateFilter.value;
+    requestData.endDate = endDateFilter.value;
+
+    if (requestData.startDate === "") {
+        requestData.startDate = "0001-01-01";
+    }
+
+    if (requestData.endDate === "") {
+        requestData.endDate = "0001-01-01";
+    }
+
+    return requestData;
+};
+
+const format = function(d) {
+    let materials = d.materials.map(material => `<td>${material.creator}</td><td>${material.description}</td><td>${material.price}</td><td class="center">${getDate(material.dateTime)}</td><td class="center">${material.isPaid}</td>`);
+    console.log(materials);
+    return `
+    <table style="width: 100%;">
+        <thead>
+            <tr>
+                <th>Creator</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Time</th>
+                <th>Paid</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>${materials.join("</tr><tr>")}</tr>
+        </tbody>
+    </table>`;
+}
+
+$(function () {
+    const detailPage = abp.appPath + 'Works/ContractorMaterialDetail';
+    const isAdmin = abp.auth.isGranted("AbpIdentity.Roles.ManagePermissions");
+
+    // abp.appPath = "https://localhost:44312/"
+    // window.abp = abp;
+
+    abp.notify.info(l("LoadingContractorMaterials"));
+    const data = abp.libs.datatables.normalizeConfiguration({
+        serverSide: true,
+        paging: true,
+        order: [[2, "asc"]],
+        searching: true,
+        scrollX: true,
+        ajax: abp.libs.datatables.createAjax(works.dispatcher.works.work.getContractorMaterials, inputAction),
+        columnDefs: [
+            {
+                className: 'dt-control',
+                orderable: false,
+                data: null,
+                defaultContent: ''
+            },
+            {
+                title: l('Contractor'),
+                data: "materialInfo",
+                className: 'left',
+                render: function (data) {
+                    return data[0];
+                    // return `<a class="padding-left-10" href="${detailPage}?id=${data[1]}" target="_blank">${data[0]}</a>`;
+                }
+            },
+            {
+                title: l('Material Price'),
+                className: 'left',
+                data: "priceStr",
+                render: function (data) {
+                    return `<div class="padding-left-10">${data}</div>`;
+                }
+            },
+            // {
+            //     title: l('Upload'),
+            //     className: 'left',
+            //     rowAction: {
+            //         items:
+            //             [
+            //                 {
+            //                     text: "Upload",
+            //                     action: function (data) {
+            //                         $("#contractorId").val(data.record.contractorId);
+            //                         $('#paymentFile').click();
+            //                         //dataTable.ajax.reload();
+            //                     }
+            //                 }
+            //             ]
+            //     }
+            // },
+            // {
+            //     title: l('View'),
+            //     className: 'left',
+            //     rowAction: {
+            //         items:
+            //             [
+            //                 {
+            //                     text: 'View',
+            //                     action: function (data) {
+            //                        
+            //                     }
+            //                 }
+            //             ]
+            //     }
+            // },
+        ],
+        drawCallback: function (settings, json) {
+            $("#MaterialsTable tbody tr").each((index, element) => {
+                let dom = $(element)
+                let color = dom.find("span[data-color]").attr("data-color");
+                dom.addClass(color);
+            });
+        }
+    });
+
+    let dataTable = $('#MaterialsTable').DataTable(data);
+
+    $('#MaterialsTable tbody').on('click', 'td.dt-control', function () {
+        let tr = $(this).closest('tr');
+        let row = dataTable.row(tr);
+        if (row.child.isShown()) {
+            row.child.hide();
+        }
+        else {
+            row.child(format(row.data())).show();
+        }
+    });
+
+    let filter = $('.filter');
+
+    filter.change(function () {
+        dataTable.draw();
+    });
+
+    $('#MaterialsTable_filter').append(filter);
+
+    startDateFilter.value = "";
+    endDateFilter.value = "";
+    
+    $("#createOrderButton").click(function () {
+        Swal.fire({
+            showCancelButton: true,
+            showConfirmButton: true,
+            showLoaderOnConfirm: true,
+            showLoading: true,
+            title: 'Now loading',
+            preConfirm: async () => {
+                return works.dispatcher.works.work.getContractorLookup();
+            },
+        }).then(result => {
+            if (result.isConfirmed) {
+                let info = result.value.items;
+                let options = {};
+
+                info.forEach(d => {
+                    options[d.id] = `${d.normalizedUserName}`
+                });
+                
+                Swal.fire({
+                    title: "Please Select Contractor",
+                    input: "select",
+                    inputOptions: options,
+                    inputPlaceholder: "Select A Contractor",
+                    showCancelButton: true,
+                }).then(result => {
+                    if (result.isConfirmed) {
+                        let productCreateModal = new abp.ModalManager({
+                            viewUrl: `/Works/ContractorMaterialCreateModal?id=${result.value}`
+                        });
+                        productCreateModal.open();
+                        productCreateModal.onResult(function(){
+                            if (arguments[1].responseText === true) {
+                                dataTable.ajax.reload();
+                            }
+                        });
+                    }
+                })
+            }
+        });
+        $(".swal2-confirm").click();
+    });
+
+    $("#paymentFileButton").click(function () {
+        $("#paymentFile").click();
+    });
+    
+    $('#paymentFile').change(function () {
+        abp.notify.info(l("Running Task"));
+        let form = $('#uploadPaymentForm');
+        form.ajaxForm({
+            dataType: "json",
+            contentType: "multipart/form-data",
+            headers: {
+                "Accept": "application/json"
+            },
+            success: function (data) {
+                if (data === "Error") {
+                    promptError("Payment File Not Correct!");
+                }
+                else {
+                    let str = data.map(x => `${x.orderId}: Actual ${x.actual}, Upstream ${x.upstream}`).join("</br>");
+                    promptHTML(str, `Mismatch Invoices: ${data.length} Items`);
+                }
+
+                $("#paymentFile").val(null);
+            }
+        });
+
+        form.submit();
+    });
+    
+    init = true;
+});
---
+++ Pages/js/Main.js
@@ -0,0 +1,180 @@
+const l = abp.localization.getResource('Dispatcher');
+const isAdmin = abp.auth.isGranted("AbpIdentity.Roles.ManagePermissions");
+const detailPage = abp.appPath + 'Works/WorkDetail';
+$(function () {
+    abp.log.debug('Main.js initialized!');
+    $(".lpx-brand-logo").html("BYDH Construction Ltd");
+});
+
+const workData = {
+    serverSide: true,
+    paging: true,
+    order: [[9, "asc"]],
+    searching: true,
+    scrollX: true,
+    columnDefs: [
+        {
+            title: l('Order'),
+            data: "orderInfo",
+            className: "freeze",
+            render: function (data) {
+                return `<div class="left"><span class="dot_${data[5]}">$</span><a href="${detailPage}?id=${data[1]}" >${data[0]}</br>${isAdmin ? data[2] : data[3]}</a></div>`;
+            }
+        },
+        {
+            title: l('Location'),
+            data: "workLocation",
+            className: "freeze freeze_2",
+            render: function (data) {
+                let location = data.address;
+                return formatLocation(location);
+            }
+        },
+        {
+            title: l('Priority'),
+            data: "priorityCode",
+            render: function (data) {
+                return `<div class="left">${data}</div>`
+            }
+        },
+        {
+            title: l('Tel'),
+            data: "tel",
+            render: function (data) {
+                return `<a href="tel:${data}">${data}</a>`;
+            }
+        },
+        {
+            title: l('IssuedActual'),
+            data: "issuedActual",
+            render: function (data) {
+                return `<div class="left padding-left-10">${getDate(data)}</div>`;
+            }
+        },
+        {
+            title: l('AppointmentTime'),
+            data: "appointmentTime",
+            render: function (data) {
+                return `<div class="left padding-left-10">${getDate(data)}</div>`;
+            }
+        },
+        {
+            title: l('Due Next'),
+            data: "workStatusInfo",
+            render: function (data) {
+                return `<div class="left">Attending: ${getDate(data.attendingDate)}</br>Completing: ${getDate(data.completingDate)}<span data-color="${data.color}"></span></div>`;
+            }
+        },
+        {
+            title: l('Requested For'),
+            data: "workRequesterName"
+        },
+        {
+            title: l('Status'),
+            data: "status",
+            render: function (data) {
+                let progress = `${data.progress}`;
+                let rest = data.total - data.elapsed;
+                return `
+                    <div>${data.status}</div>
+                    <div class="progress">
+                        <p style="position: absolute;width: 66px;">${rest} Days</p>
+                        <div class="progress-bar progress-bar-striped" role="progressbar" style="width: ${progress}%" aria-valuenow="${progress}" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>`;
+            }
+        },
+        {
+            title: l('Edit'),
+            className: "edit",
+            orderable: false,
+            rowAction: {
+                items:
+                    [
+                        {
+                            text: null,
+                            icon: 'trash',
+                            visible: abp.auth.isGranted('Dispatcher.Works.Delete'),
+                            confirmMessage: function (data) {
+                                return l('WorkDeletionConfirmationMessage', data.record.name);
+                            },
+                            action: function (data) {
+                                works.dispatcher.works.work.delete(data.record.id).then(function () {
+                                    abp.notify.info(l('SuccessfullyDeleted'));
+                                    dataTable.ajax.reload();
+                                });
+                            }
+                        }
+                    ]
+            }
+        },
+        {
+            title: l('Check'),
+            data: "workInfo",
+            className: "checkbox",
+            orderable: false,
+            render: function (data) {
+                return `<input type='checkbox' class='checkbox' data-work='${data.id}' data-price='${data.totalPrice}'/>`;
+            }
+        }
+    ],
+    drawCallback: function (settings, json) {
+        $("#WorksTable tbody tr").each((index, element) => {
+            let dom = $(element)
+            let color = dom.find("span[data-color]").attr("data-color");
+            dom.addClass(color);
+        });
+    }
+};
+
+const formatLocation = function(location) {
+    let tmp = location.split("");
+    let words = 25
+    let index = Math.floor(tmp.length / words);
+    for (let i = 1; i <= index; i++) {
+        tmp[i * words] += "</br>";
+    }
+    return `<div class='left'>${tmp.join("")}</div>`;
+}
+const getDate = function (str) {
+    if (str === null || str === "0" || str.includes("0001") || !str) return "Not Available";
+    return (new Date(str)).toLocaleString("en-NZ", {
+        year: "numeric",
+        month: "short",
+        day: "2-digit",
+    });
+};
+
+const getSearch = function (param) {
+    let params = new URLSearchParams(location.search);
+    return params.get(param);
+}
+
+const promptError = function (message) {
+    Swal.fire({
+        title: 'Error',
+        text: l(message),
+        icon: 'error',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'OK',
+    });
+}
+
+const promptOK = function (message) {
+    Swal.fire({
+        title: 'Success',
+        text: l(message),
+        icon: 'success',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'OK',
+    });
+}
+
+const promptHTML = function (message, title) {
+    Swal.fire({
+        title: title,
+        html: `<div class="swalInfo">${message}</div>`,
+        icon: 'info',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'OK',
+    });
+}
