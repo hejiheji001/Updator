e67791225ba9a2547baff4a3e4c288fecdc5f99e
FireAwayH <hejiheji001@gmail.com>
2025-05-19 02:03:59 +0800

    update

---
+++ Pages/js/PaymentInvoice.js
@@ -0,0 +1,21 @@
+$(function () {
+    $("#printButton").click(function () {
+        $("#invoice").print();
+    });
+    
+    $("#payButton").click(function () {
+        let form = $("#payForm");
+        form.ajaxForm({
+            dataType: "json",
+            contentType: "multipart/form-data",
+            headers: {
+                "Accept": "application/json"
+            },
+            success: function (data) {
+                promptOK("All Invoices Set To Paid.")
+            }
+        });
+
+        form.submit();
+    })
+});
